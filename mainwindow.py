cc8cd8607d27c9fe8f4cf0643bdc1327c5f21206
lock <lockbeW@gmail.com>
2023-12-26 15:53:09 +0300

    Stable

--- mainwindow.py
+++ mainwindow.py
@@ -1,7 +1,7 @@
-from email import message
 import sys
 import sqlite3
 import datetime
+import re
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QMessageBox
@@ -19,7 +19,7 @@ class DatabaseController:
         
     def addEmplInfo(self, emplInfo: tuple):
             with self.connection as con:
-                return con.cursor().execute('INSERT INTO emplInf VALUES (?,?,?,?,?,?,?,?)', emplInfo)
+                return con.cursor().execute('INSERT INTO emplInf VALUES (?,?,?,?,?,?,?,?,?,?)', emplInfo)
             
     def getEmplInfo(self, iid: int):
         with self.connection as con:
@@ -114,27 +114,30 @@ class Ui_addWindow(MessageHandler, DatabaseController):
         self.phoneNumberLine.setObjectName("phoneNumberLine")
         
 
-        self.tempEmplCB = QtWidgets.QCheckBox(parent=self.centralwidget)
-        self.tempEmplCB.setGeometry(QtCore.QRect(50, 155, 131, 25))
-        self.tempEmplCB.setObjectName("tempEmplCB")
-        
-
         self.accesEndingLine = QtWidgets.QLineEdit(parent=self.centralwidget)
         self.accesEndingLine.setEnabled(False)
         self.accesEndingLine.setGeometry(QtCore.QRect(190, 155, 131, 25))
         self.accesEndingLine.setText("")
         self.accesEndingLine.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.accesEndingLine.setObjectName("accesEndingLine")
-        
 
+
+        self.tempEmplCB = QtWidgets.QCheckBox(parent=self.centralwidget)
+        self.tempEmplCB.setGeometry(QtCore.QRect(50, 155, 131, 25))
+        self.tempEmplCB.setObjectName("tempEmplCB")
+        self.tempEmplCB.toggled.connect(self.setAccesEndingLine)
+
+        
         self.delEmplBtn = QtWidgets.QPushButton(parent=self.centralwidget)
         self.delEmplBtn.setGeometry(QtCore.QRect(60, 189, 261, 25))
         self.delEmplBtn.setObjectName("delEmplBtn")
+        self.delEmplBtn.clicked.connect(self.delEmpl)
         
 
         self.addEmplBtn = QtWidgets.QPushButton(parent=self.centralwidget)
         self.addEmplBtn.setGeometry(QtCore.QRect(60, 220, 261, 25))
         self.addEmplBtn.setObjectName("addEmplBtn")
+        self.addEmplBtn.clicked.connect(self.addEmpl)
         
 
         addWindow.setCentralWidget(self.centralwidget)
@@ -151,11 +154,98 @@ class Ui_addWindow(MessageHandler, DatabaseController):
         self.birhdayLine.setPlaceholderText(_translate("addWindow", "Дата рождения"))
         self.jobtitleLine.setPlaceholderText(_translate("addWindow", "Должность"))
         self.phoneNumberLine.setPlaceholderText(_translate("addWindow", "Номер телефона"))
-        self.tempEmplCB.setText(_translate("addWindow", "Временный сотрудник"))
+        self.tempEmplCB.setText(_translate("addWindow", "Временный пропуск"))
         self.accesEndingLine.setPlaceholderText(_translate("addWindow", "Дата отзыва пропуска"))
         self.delEmplBtn.setText(_translate("addWindow", "Удалить сотрудника (задействуется только ID)"))
         self.addEmplBtn.setText(_translate("addWindow", "Добавить сотрудника"))
         self.addEmplBtn.setShortcut(_translate("addWindow", "Return"))
+        
+    def addEmplInfoIsValid(self):
+        if len(self.firstNameLine.text().strip()) == 0 or len(self.firstNameLine.text().strip().split()) != 1:
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Имя введено некоректно.')
+            return False
+        
+        if len(self.secondNameLine.text().strip()) == 0 or len(self.secondNameLine.text().strip().split()) != 1:
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Фамилия введена некоректно.')
+            return False    
+        
+        if len(self.lastNameLine.text().strip()) == 0 or len(self.lastNameLine.text().strip().split()) != 1:
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Отчество введено некоректно.')
+            return False     
+        
+        if re.match(r'(0?[1-9]|[12][0-9]|3[01]).(0?[1-9]|1[012]).((19|20)\d\d)', self.birhdayLine.text().strip()) is None or len(self.birhdayLine.text().strip().split()) > 1:
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Дата рождения введена некоректно. Она должна быть в формате дд/мм/гггг')
+            return False
+        
+        if len(self.jobtitleLine.text().strip()) == 0 or len(self.jobtitleLine.text().strip().split()) > 4:
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Должность введена некоректно.')
+            return False
+                    
+        if re.fullmatch(r'\+7\d{10}', self.phoneNumberLine.text().strip()) is None or len(self.phoneNumberLine.text().strip()) != 12:
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Номер телефона введён некоректно. Он должен быть в формате +7**********')
+            return False
+        
+        if self.tempEmplCB.isChecked():
+            if re.match(r'(0?[1-9]|[12][0-9]|3[01]).(0?[1-9]|1[012]).((19|20)\d\d)', self.accesEndingLine.text().strip()) is None or len(self.accesEndingLine.text().strip().split()) > 1:
+                self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Дата отзыва пропуска введена некоректно. Она должна быть в формате дд/мм/гггг')
+                return False
+        
+        return True
+       
+    def setAccesEndingLine(self):
+        self.accesEndingLine.setEnabled(self.tempEmplCB.isChecked())
+    
+    def delEmpl(self):
+        if self.idAddLine.text() == '':
+            return
+        
+        inputid = int(self.idAddLine.text())        
+        if inputid not in range(1000, 10000):
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'ID должен состоять из 4 цифр.')
+            self.idAddLine.clear()
+            return
+        
+        if self.checkId(inputid):
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Сотрудника с таким ID не существует.')
+            self.idAddLine.clear()
+            return
+        
+        self.deleteRow(inputid)
+        self.show_message(QMessageBox.Icon.NoIcon, ' ', 'Сотрудник удалён из базы данных.')
+    
+    def addEmpl(self):
+        if self.idAddLine.text() == '':
+            return
+        
+        inputid = int(self.idAddLine.text())        
+        if inputid not in range(1000, 10000):
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'ID должен состоять из 4 цифр.')
+            self.idAddLine.clear()
+            return
+        
+        if not self.checkId(inputid):
+            self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Сотрудник с таким ID уже существует.')
+            self.idAddLine.clear()
+            return
+        
+        if self.addEmplInfoIsValid():
+            if not self.tempEmplCB.isChecked(): addAccesEndingLine = 'не ожидается' 
+            else: addAccesEndingLine = self.accesEndingLine.text()
+            
+        addId = int(self.idAddLine.text().strip())
+        addSN = self.secondNameLine.text().strip()
+        addFN = self.firstNameLine.text().strip()
+        addLN = self.lastNameLine.text().strip()
+        addBL = self.birhdayLine.text().strip()
+        addJL =  self.jobtitleLine.text().strip()
+        addPN = self.phoneNumberLine.text().strip().replace('+', '')
+        addLV = 'Никогда'
+        addOT = 0
+        
+        inputInfo = (addId, addSN, addFN, addLN, addBL, addJL, addPN, addLV, addAccesEndingLine, addOT)
+        
+        self.addEmplInfo(inputInfo)
+        self.show_message(QMessageBox.Icon.NoIcon, ' ', 'Сотрудник добавлен в базу данных.')
         
 class Ui_mainWindow(Ui_addWindow):
     def setupUi(self, mainWindow):
@@ -225,12 +315,11 @@ class Ui_mainWindow(Ui_addWindow):
         return False
     
     def showAddWindow(self):
-        addWindow = QtWidgets.QMainWindow()
-        ui = Ui_addWindow()
-        ui.setupUi(addWindow)
-        addWindow.show()
-        sys.exit(app.exec())
-        
+        self.add = QtWidgets.QMainWindow()
+        self.ui = Ui_addWindow()
+        self.ui.setupUi(self.add)
+        self.add.show()
+
     def accessEmpl(self):
         if self.unvalid():
             return
@@ -242,7 +331,7 @@ class Ui_mainWindow(Ui_addWindow):
         if self.getEmplPos(inputid): message = f'Сотрудник с ID {inputid} вошёл на территорию.'
         else: message = f'Сотрудник с ID {inputid} покинул территорию.'
             
-        self.show_message(QMessageBox.Icon.NoIcon, 'Сотрудник пропущен', message)
+        self.show_message(QMessageBox.Icon.NoIcon, ' ', message)
         
     def showEmplInformation(self):
         if self.unvalid():
